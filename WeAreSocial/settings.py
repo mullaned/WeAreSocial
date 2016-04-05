81073534b80f97b92d7029e1b1f12a9598986ca9
mullaned <mullaned@gmail.com>
2016-04-05 15:25:55 +0100

    blog added

--- WeAreSocial/settings.py
+++ WeAreSocial/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'WeAreSocial',
     'WeAreSocial_app',
+    'blog',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -122,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, "static"),
+)
--- WeAreSocial_app/views.py
+++ WeAreSocial_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from WeAreSocial_app.models import Contact
+# Create your views here.
 
 # Create your views here.
+def get_contacts(request):
+    return render(request, "WeAreSocial/home.html",
+                  {'contact_list': Contact.objects.all()})
