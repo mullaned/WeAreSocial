0034fc4cc494ae761f6628e465a8641beb675b9d
mullaned <mullaned@gmail.com>
2016-04-05 10:46:03 +0100

    initial commit

---
+++ WeAreSocial_app/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Contact(models.Model):
+
+    class Meta:
+        app_label = "WeAreSocial_app"
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    mobile = models.CharField(max_length=255)
+
+    def __str__(self):
+        return ' '.join([
+            self.first_name,
+            self.last_name,
+        ])
+
